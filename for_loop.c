7c07d2ffd2381583fb6dfe4d5a6c685737a1d323
G Shree Raksha <g.raksha@s.amity.edu>
2026-03-11 22:53:30 +0530

    displaying one even and one odd using for loop

--- for_loop.c
+++ for_loop.c
@@ -2,19 +2,26 @@
 
 int main() 
 {
-    int a, b,i;
-    int even, odd;
-    scanf("%d\n%d", &a, &b);
-    for(i=a;i<=b;i++){
-  	if(i==8)printf("eight\n");
-    else if(i==9)printf("nine\n");
-    else if(i%2==0)
+    int a,b,i;
+    scanf("%d%d",&a,&b);
 
+    for(i=a;i<=b;i++)
+    {
+        if(i==1) printf("one\n");
+        else if(i==2) printf("two\n");
+        else if(i==3) printf("three\n");
+        else if(i==4) printf("four\n");
+        else if(i==5) printf("five\n");
+        else if(i==6) printf("six\n");
+        else if(i==7) printf("seven\n");
+        else if(i==8) printf("eight\n");
+        else if(i==9) printf("nine\n");
+
+        else if(i%2==0)
             printf("even\n");
-            else 
-            printf("odd");
-        
+        else
+            printf("odd\n");
     }
+
     return 0;
 }
-
